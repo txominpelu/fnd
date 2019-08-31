6bf944cf618688c4107f1fef84c314a465a8eafe
Inigo Mediavilla Saiz <inigo.mediavilla@booking.com>
2019-08-31 12:53:02 +0200

    jump directly to apply on browser

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-python run.py list  | fzf --preview 'python run.py description {1}' --preview-window hidden --bind ctrl-k:preview-up,ctrl-j:preview-down,?:toggle-preview
+guid=$(python run.py list  | fzf --preview 'python run.py description {1}' --preview-window hidden --bind ctrl-k:preview-up,ctrl-j:preview-down,?:toggle-preview | cut -d '.' -f 1)
+xdg-open "https://remoteok.io/l/$guid"
