f738afb6bac9c256fc9f6096553a6284d46bc19f
Txomin Pelu <txominpelu@gmail.com>
2019-10-03 13:59:10 +0200

    changed description

--- cmd/root.go
+++ cmd/root.go
@@ -16,11 +16,9 @@ import (
 
 var RootCmd = &cobra.Command{
 	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
-	Run: runRoot,
+	Short: "Clone of fzf with extended features",
+	Long:  `Clone of fzf with extended features`,
+	Run:   runRoot,
 }
 
 func init() {
